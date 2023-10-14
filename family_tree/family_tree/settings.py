8388b277513060694b16af804fc087c540a54a4b
Prometheus <poligraphich@gmail.com>
2023-10-14 19:19:13 +0300

    extract to web

--- family_tree/family_tree/settings.py
+++ family_tree/family_tree/settings.py
@@ -9,27 +9,37 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import os
+
 from pathlib import Path
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u@n1v-tp(rqwbj=6o&d%4xl8sbewf&f(j&!j*-tt887aygs@r%'
+# SECRET_KEY = 'django-insecure-u@n1v-tp(rqwbj=6o&d%4xl8sbewf&f(j&!j*-tt887aygs@r%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
 
-'127.0.0.1'
+    '127.0.0.1',
+    'Prometheus844.pythonanywhere.com',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -41,9 +51,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'my_shop',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,17 +85,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'family_tree.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'Prometheus844$default',
+        'USER': '<Prometheus844>',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'Prometheus844.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",
+                       'charset': 'utf8mb4',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -103,7 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -115,12 +131,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -168,4 +183,3 @@ LOGGING = {
         },
     },
 }
-
--- family_tree/family_tree/urls.py
+++ family_tree/family_tree/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from my_shop.views import upload_image
+
 urlpatterns = [
-path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('my_shop.urls')),
     path('my_shop/', include('my_shop.urls')),
-path('upload/', upload_image, name='upload_image'),
-        ]
+    path('upload/', upload_image, name='upload_image'),
+    # path('__debug__/', include("debug_toolbar.urls")),
+]
